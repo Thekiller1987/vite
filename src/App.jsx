d224c21774bf45bff815aa875a0f8e9d3ad2442a
waskar <waskareliasrios11@gmail.com>
2025-03-10 10:39:06 -0600

    clase 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
-import './App.css';
-import TemperatureConverter from './TemperatureConverter';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main className="container"> 
-      <div className="header">
-        <h1>Conversor de Temperatura</h1>
-      </div>
-
-      <div className="content">
-        <TemperatureConverter />
-
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
+    return (
+        <div>
+            <h1>Componente principal</h1>
         </div>
-      </div>
-
-      <footer className="footer">
-        <p>
-          Click on the Vite and React logos to learn more
-        </p>
-      </footer>
-    </main>
-  );
+    );
 }
 
 export default App;
