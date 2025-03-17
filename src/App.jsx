3ac2a5efafb13cd74638bc7b4456488c6491ad49
waskar <waskareliasrios11@gmail.com>
2025-03-17 09:51:02 -0600

    adiciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
-import ProtectedRoute from "./components/ProtectedRoute"; 
-import Login from './views/Login'
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from './views/Login';
 import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
-
-import './App.css'
+import Categorias from "./views/Categorias"; // Importación de Categorias
+import './App.css';
 
 function App() {
-
   return (
     <>
       <AuthProvider>
@@ -17,17 +16,16 @@ function App() {
             <Encabezado />
             <main>
               <Routes>
-                
                 <Route path="/" element={<Login />} />
                 <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
-
+                <Route path="/categorias" element={<ProtectedRoute element={<Categorias />} />} /> // Ruta de Categorias protegida
               </Routes>
             </main>
           </div>
         </Router>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
